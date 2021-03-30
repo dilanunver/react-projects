0bad47dc474d774c742ab92c9e317e8a88bfe2c7
dilanunver <dunver33@gmail.com>
2021-03-30 15:38:00 +0300

    accordion project

--- 4-accordion/setup/src/Question.js
+++ 4-accordion/setup/src/Question.js
@@ -1,7 +1,18 @@
 import React, { useState } from 'react';
 import { AiOutlineMinus, AiOutlinePlus } from 'react-icons/ai';
-const Question = () => {
-  return <h2>question component</h2>;
+const Question = ({ info, title }) => {
+  const [toggle, setToggle] = useState(false)
+  return (
+    <article className='question'>
+      <header>
+        <h4>{title}</h4>
+        <button className='btn' onClick={() => setToggle(!toggle)}>
+          {toggle ? <AiOutlinePlus></AiOutlinePlus> : <AiOutlineMinus></AiOutlineMinus>}
+        </button>
+      </header>
+      {toggle ? <p>{info}</p> : ''}
+    </article>
+  );
 };
 
 export default Question;
